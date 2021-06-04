7a454820f5a7c8caa952296d8b2c94f3e8b69ae9
Desiree Zalm <82588366+desireezalm@users.noreply.github.com>
2021-06-04 13:23:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hallo Winc Academy studenten")
+//Comments zijn niet zichtbaar op de console, omdat Node ze niet leest.
+//Dit is een grote som 
+/*let multiplication = 1230941 * 1823794;
+console.log(multiplication)
+let division = 637263 / 54;
+console.log(division)*/
